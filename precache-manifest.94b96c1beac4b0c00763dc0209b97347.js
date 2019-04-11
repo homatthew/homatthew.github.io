c9eca66e38b47a1e9310da2d73243e4260a97bcd
homatthew <matthewho@umail.ucsb.edu>
2019-04-10 22:52:44 -0700

    Updates

---
+++ precache-manifest.94b96c1beac4b0c00763dc0209b97347.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9aee55b7a24a67c4365",
+    "url": "/static/js/main.eafb7551.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e9aee55b7a24a67c4365",
+    "url": "/static/css/main.474f0275.chunk.css"
+  },
+  {
+    "revision": "40ed92000418a3771a92e1af6c9ac831",
+    "url": "/index.html"
+  }
+];
